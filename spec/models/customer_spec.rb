2438cc7506819054f4a917ada0110f19c7077f63
marceltheshell <marcel.degas@gmail.com>
2018-03-06 15:34:17 -0800

    finishes tests for customer_spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,16 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
 
-	context 'associations' do
-		it 'has many orders' do
-			should have_many(:orders)
-		end
-	end
+  context 'associations' do
+    it { should have_many(:orders) }
+  end
 
-	context 'validations' do
-		it 'validates email for uniqueness' do
-			Customer.create!(email: "unique@u.com")
-			expect{ Customer.create!(email: "unique@u.com") }.to raise_error(ActiveRecord::RecordInvalid)
-		end
-	end
+  context 'validations' do
+    
+    before do
+      @customer = Customer.create!(email: "g@g.co")
+      3.times { Order.create!(customer: @customer) }
+    end
+
+    it "destroys the associated products on self destruct" do
+        orders = @customer.orders
+        @customer.destroy
+        orders.each do |order|
+          expect(Order.find(order)).to raise_error ActiveRecord::RecordNotFound
+        end
+    end
+
+    it 'validates email for uniqueness' do
+      Customer.create!(email: "unique@u.com")
+      expect{ Customer.create!(email: "unique@u.com") }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+  end
 end
