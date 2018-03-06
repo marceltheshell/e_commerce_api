f566fcd0ef0206013d2285f1b759b0f5497ef16c
marceltheshell <marcel.degas@gmail.com>
2018-03-05 17:51:38 -0800

    returns orders in customer controller

--- app/controllers/api/customer_controller.rb
+++ app/controllers/api/customer_controller.rb
@@ -1,5 +1,6 @@
 class Api::CustomerController < ApplicationController
   def show
-    
+    customer_orders = Customer.find(params[:id]).orders
+    render json: customer_orders.as_json, status: :ok
   end
 end
